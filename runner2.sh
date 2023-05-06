b3bda00aae769e5a713de89107796926bb83be14
damanimehul <48509266+damanimehul@users.noreply.github.com>
2023-05-05 23:06:17 -0400

    run more target class attacks on clip

--- runner2.sh
+++ runner2.sh
@@ -1,1 +1,1 @@
-python main.py --name maxem-v2-5000-lr1e-2 --wandb  --device cuda --dataset 5000 --log_freq 50 --lr 1e-2 --epochs 1000 --batch_size 128 --trainer max_embedding_trainer
+python main.py --name target-tv-5000-lr1e-1 --wandb  --device cuda --dataset 5000 --log_freq 25 --lr 1e-1 --epochs 1000 --batch_size 128 --target_class tv
