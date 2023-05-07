331d7d9a428bd1a5505d892fc55b4b30e1d93217
damanimehul <48509266+damanimehul@users.noreply.github.com>
2023-05-06 21:58:06 -0400

    try more target class attacks

--- runner.sh
+++ runner.sh
@@ -1,1 +1,1 @@
-python main.py --name target-airplane-5000-lr1e-1 --wandb  --device cuda --dataset 5000 --log_freq 25 --lr 1e-1 --epochs 1000 --batch_size 128 --target_class airplane
+python main.py --name target-refrigerator-5000-lr1e-1 --wandb  --device cuda --dataset 5000 --log_freq 25 --lr 1e-1 --epochs 1000 --batch_size 128 --target_class refrigerator
--- runner2.sh
+++ runner2.sh
@@ -1,1 +1,1 @@
-python main.py --name target-elephant-5000-lr1e-1 --wandb  --device cuda --dataset 5000 --log_freq 25 --lr 1e-1 --epochs 1000 --batch_size 128 --target_class elephant
+python main.py --name target-elephant-5000-lr1e-1 --wandb  --device cuda --dataset 5000 --log_freq 25 --lr 1e-1 --epochs 1000 --batch_size 128 --target_class knife
